c5b6c2732f81d4c4089035c009d2b4b3951ab0f3
JK1331 <jakubkubala3@gmail.com>
2022-09-18 15:25:00 +0200

    init test variables

--- test/StakingPool.test.ts
+++ test/StakingPool.test.ts
@@ -7,3 +7,27 @@ import { deployContract, loadFixture, MockProvider, solidity } from "ethereum-wa
 import  StakingPoolContract from "../artifacts/contracts/StakingPool.sol/StakingPool.json";
 
 use(solidity);
+
+describe("Staking Pool", function () {
+    const oneETH = utils.parseUnits("1", "ether");
+    const hardCap = oneETH.mul(5000000);
+    const contributionLimit = oneETH.mul(50000);
+    const ratio = 0.0000225;
+    const ratioInt = utils.parseUnits(ratio.toString(), 18); 
+    const defaultRoleVersion = 1;
+    const patronRoleDef = utils.namehash("email.roles.verification.app.stakingPool.iam.ewc");
+    const ownerRoleDef = utils.namehash("owner.roles.staking.app.stakingPool.iam.ewc");
+    const timeTravel = async (provider: MockProvider, seconds: number) => {
+        await provider.send("evm_increaseTime", [seconds]);
+        await provider.send("evm_mine", []);
+    };
+
+    async function stakeAndTravel(stakingPool: StakingPool, value: BigNumber, seconds: number, provider: any) {
+        await stakingPool.stake({ value });
+        await timeTravel(provider, seconds);
+    }
+
+    
+
+
+})
